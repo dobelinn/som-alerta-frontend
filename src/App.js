630b790c64ebe8b9d73cb12296af279f2a12354d
Bruno Dobelin <bruno.dobelin13@gmail.com>
2025-04-16 00:04:14 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import './App.css';
+
+function App() {
+  const [alerta, setAlerta] = useState(false);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3000');
+
+    socket.on('alerta', () => {
+      setAlerta(true);
+      setTimeout(() => setAlerta(false), 5000); // piscar por 5s
+    });
+
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="quarto-wrapper">
+        <img src="/quarto-hospital.png" alt="Quarto de hospital" className="quarto-img" />
+        {alerta && <div className="alerta-piscando" />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
